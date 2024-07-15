aa069c1cd1bf0a57f852a5553908fb326644cdc4
FrancisLauriano <130801505+FrancisLauriano@users.noreply.github.com>
2024-07-15 02:18:43 -0300

    ok

---
+++ scripts_database_table/troca_livros.sql
@@ -0,0 +1,13 @@
+-- Database: troca_livros
+
+-- DROP DATABASE IF EXISTS troca_livros;
+
+CREATE DATABASE troca_livros
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Portuguese_Brazil.1252'
+    LC_CTYPE = 'Portuguese_Brazil.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
